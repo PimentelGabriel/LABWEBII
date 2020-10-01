49d41da116821571e39ef49e737373fe37a01115
unknown <gabrielcota,tads@gmail.com>
2020-10-01 08:39:36 -0300

    Reorganizacao de repositorio

---
+++ 30-09 Quarta/aula01/a01p03.php
@@ -0,0 +1,12 @@
+<?php
+$ingredientes = [
+    'açicar',
+    'farinha de trigo',
+    'fermento'
+];
+
+$numeroDaMengasena = [10, 14, 45, 87, 76]
+
+echo $numeroDaMengasena[0]."<br><br>";
+echo $numeroDaMengasena[1]."<br><br>";
+echo $numeroDaMengasena[2]."<br><br>";
